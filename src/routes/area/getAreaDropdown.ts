3ab41a785eb80a0820f291fb8be2e157e16987b3
Giorgos Papapanos <g.papapanos1996@gmail.com>
2025-05-08 18:50:09 +0300

    feat: add getAreasDropdown route for area selection

---
+++ src/routes/area/getAreaDropdown.ts
@@ -0,0 +1,22 @@
+import { Hono } from "hono";
+import { db } from "../../db";
+import { languages, area_translations, areas } from "../../db/schema";
+import { eq } from "drizzle-orm";
+
+export const getAreasDropdown = new Hono().get("/dropdown", async (c) => {
+  const acceptLanguage = c.req.header("Accept-Language") || "en"; // Default to 'en' if not provided
+
+  const result = await db
+    .select({
+      id: areas.id,
+      title: area_translations.title,
+    })
+    .from(areas)
+    .leftJoin(area_translations, eq(areas.id, area_translations.areaId))
+    .leftJoin(languages, eq(languages.id, area_translations.languageId))
+    .where(eq(languages.locale, acceptLanguage));
+
+  return c.json({
+    items: result,
+  });
+});
--- src/routes/area/index.ts
+++ src/routes/area/index.ts
@@ -2,11 +2,12 @@ import { Hono } from "hono";
 import { getAreas } from "./getArea";
 import { createArea } from "./createArea";
 import { deleteArea } from "./deleteArea";
-
+import { getAreasDropdown } from "./getAreaDropdown";
 const areaRoutes = new Hono();
 
 areaRoutes.route("/", getAreas);
 areaRoutes.route("/", createArea);
 areaRoutes.route("/", deleteArea);
+areaRoutes.route("/", getAreasDropdown);
 
 export { areaRoutes };
