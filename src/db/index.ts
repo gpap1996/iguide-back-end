29c825ceca24a384fbbb791cb0c6263651e8c073
Giorgos Papapanos <g.papapanos1996@gmail.com>
2025-05-25 14:32:46 +0300

    fix: supa env

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,6 +3,8 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 import * as schema from "./schema";
 
+console.log(`Connecting to database at ${process.env.DATABASE_URL}`);
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL!,
   ssl: false,
